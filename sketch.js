51d28e0cfa371f7a626abaa4f2948185a3c310a1
LuciaAndrea <43391254+LuciaAndrea@users.noreply.github.com>
2018-10-02 22:26:21 +0200

    Rotation

--- sketch.js
+++ sketch.js
@@ -1,11 +1,44 @@
 function preload(){
   // put preload code here
 }
-
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background(189,250,100);
+  angleMode(DEGREES);
+
+  frameRate(120);
 }
 
 function draw() {
-  // put drawing code here
+
+
+  translate(width/2, height/2);
+rotate(frameCount*9);
+  strokeWeight(1);
+      noFill();
+      var angleOK = frameCount*1;
+      var xyeah = 300*cos(angleOK);
+      var yyeah= 300*sin(angleOK);
+
+      stroke(lerpColor(color('#ea0043'), color('#000000'), frameCount/60));
+     stroke(30, 100, 250);
+
+      line(xyeah,yyeah,120,0);
+
+
+if(frameCount==240) {
+  noLoop();
 }
+
+
+
+
+
+
+
+//
+
+
+
+
+   }
